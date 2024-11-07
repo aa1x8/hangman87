d0bf82a8e9536f700e1904a5065702c0d8bdc511
aa1x8 <aminasah333@gmail.com>
2024-11-07 19:46:34 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -4,4 +4,20 @@ import random
 word_list = ['pomergranate', 'mango', 'banana', 'apple', 'strawberries']
 print(word_list)
 
+# Create a random word from list
 def choose_random_word(word_list):
+  print(word)
+  
+favorite_fruits = ['pomegranate', 'mango', 'banana', 'apple', 'strawberries']
+word_list = favorite_fruits
+word = random.choice(word_list)
+print("Word List:", word_list)
+print("Randomly chosen word:", word)
+
+#task 3
+def get_player_guess():
+    """Ask the user to input a single letter."""
+    return input('Please enter a single letter: ')
+
+
+
